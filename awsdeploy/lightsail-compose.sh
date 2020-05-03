38367cfca03de44f10e059fceb22c69ac0a175b8
amcgeough <andrewmcgeough@gmail.com>
2020-05-03 19:10:23 +0100

    pull down addons folder

--- awsdeploy/lightsail-compose.sh
+++ awsdeploy/lightsail-compose.sh
@@ -14,12 +14,16 @@ chmod +x /usr/local/bin/docker-compose
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose.yml
+curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/amcgeough/mortimer/master/awsdeploy/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
+curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/amcgeough/mortimer/master/awsdeploy/docker-compose-app.service
 systemctl enable docker-compose-app
+
+# copy and unzip addons folder
+curl -o addons.zip -L https://github.com/amcgeough/mortimer/raw/master/awsdeploy/addons.zip
+unzip addons.zip
 
 # start up the application via docker-compose
 docker-compose -f /srv/docker/docker-compose.yml up -d
