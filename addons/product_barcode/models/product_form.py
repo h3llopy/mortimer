4b8ae8dc7e6f4da3c17ce8fd3fe4f9c80dd660c0
amcgeough <andrewmcgeough@gmail.com>
2020-05-05 23:42:41 +0100

    add comment templates.barcode

--- addons/product_barcode/models/product_form.py
+++ addons/product_barcode/models/product_form.py
@@ -92,7 +92,7 @@ class ProductTemplateAutoBarcode(models.Model):
     def create(self, vals_list):
         templates = super(ProductTemplateAutoBarcode, self).create(vals_list)
         ean = generate_ean(str(templates.id))
-        templates.barcode = ean
+        # templates.barcode = ean
         templates.barcode = ean if templates.barcode == False else templates.barcode
         return templates
 
